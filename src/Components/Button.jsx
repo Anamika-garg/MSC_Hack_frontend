019ad8e5ef53b59616e37f59730feea35a08f198
Anamika Garg <anamikagarg2906@gmail.com>
2025-02-08 11:26:13 +0530

    resposnive

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ children, onClick, className }) => {
     return (
       <button 
         onClick={onClick} 
-        className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${className}`}
+        className={`px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 ${className}`}
       >
         {children}
       </button>
