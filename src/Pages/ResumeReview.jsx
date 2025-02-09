d98cb1cc4c0d131ef67abdc5f02d8a6331781271
Anamika Garg <anamikagarg2906@gmail.com>
2025-02-09 22:02:28 +0530

    deployemt resolved

--- src/Pages/ResumeReview.jsx
+++ src/Pages/ResumeReview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import PdfToText from 'react-pdftotext';
+import pdfToText from 'react-pdftotext';
 
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -18,7 +18,7 @@ export default function ResumeReview() {
       setFile(selectedFile);
       setIsProcessing(true); // Start processing when the file is uploaded
 
-      PdfToText(selectedFile)
+      pdfToText(selectedFile)
         .then(text => {
           setData(text);
           setIsProcessing(false); // Text extraction complete
